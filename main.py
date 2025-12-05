3bbba513315d8ff4b352ef5437fada1200234d36
PPzazaskbd <lada3452117@gmail.com>
2025-12-06 00:27:04 +0700

    add count char and update output style

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-from stats import count_words
+from stats import * 
 
 
 def main():
     filepath = "books/frankenstein.txt"
     
     num = count_words(filepath)
+    chars = count_chars(filepath)
+    list_chars = dict_to_list(chars)
+    list_chars.sort(reverse=True , key=sort_on)    
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
     print(f"Found {num} total words")
-    return None
+    print("--------- Character Count -------")
+    
+    for list in list_chars:
+        if list["char"].isalpha() == False:
+            continue
+        print(f"{list["char"]}: {list["num"]}")
+
+
+
+
+    print("============= END ===============")
+    return None 
 
 
 
--- stats.py
+++ stats.py
@@ -10,3 +10,23 @@ def count_words(text):
     return len(get_book_text(text).split())
 
 
+def count_chars(text):
+    dict = {}
+    book = get_book_text(text)
+    for char in book:
+        char = char.lower()
+        if char not in dict:
+            dict[char] = 0
+        dict[char] += 1
+    return dict
+                 
+def dict_to_list(dict):
+    list =[]
+    for k,v in dict.items():
+        list.append({
+            "char" : k ,"num" : v
+        })
+    return list
+
+def sort_on(item):
+    return item["num"]
